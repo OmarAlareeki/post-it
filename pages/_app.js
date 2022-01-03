a7a382e70bb2ad6fe8c501faeedc698abda0fbe5
OmarAlareeki <omarproductions2018@gmail.com>
2022-01-03 01:36:00 -0800

    base repo for post it

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import Head from 'next/head'
+import fire from '../config/fire-config';
+
+function MyApp({ Component, pageProps }) {
+  console.log()
+  return (
+  <>
+    <Head>
+      <title>Post It </title>
+      <meta name='description' content='This app helps to keep track of orders'/>
+      <link rel="icon" href="/post_it.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+  )
+}
+
+export default MyApp;
