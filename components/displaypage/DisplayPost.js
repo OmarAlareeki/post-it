e056c2e7aab3f078fcb1461b4c827caf5522fbc9
NGFuture <78330887+NGFuture@users.noreply.github.com>
2022-01-16 21:40:13 -0800

    map, gallery

--- components/displaypage/DisplayPost.js
+++ components/displaypage/DisplayPost.js
@@ -1,20 +1,28 @@
 import React from "react";
 import Content from "./Content";
-import Map from "./Map";
+// import Map from "./Map";
 import PhotoGallery from "./PhotoGallery";
+import styles from "./DisplayPost.module.css";
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(
+  () => import('./Map'),
+  { ssr: false }
+)
+
 
 
 export default function DisplayPost({ post }) {
 //  return 'check'
     return (
-        <div>
-            <div>
-                <Content />
+        <div className={styles.mainField}>
+            <div className={styles.content}>
+                <Content post={post} />
             </div>
-            {/* <div>
-                <Map />
-            </div> */}
-            <div>
+            <div className={styles.map}>
+                <Map zip={post.zip}/>
+            </div>
+            <div className={styles.photoGallery}>
                 <PhotoGallery photos={post.imageUrls} />
             </div>
         </div>
--- components/displaypage/PhotoGallery.js
+++ components/displaypage/PhotoGallery.js
@@ -1,21 +1,21 @@
 import React from "react";
 import styles from "./PhotoGallery.module.css";
+import { Carousel } from 'react-bootstrap'
 
-export default function PhotoGallery ({photos}) {
-// for testing doll's url in FB https://firebasestorage.googleapis.com/v0/b/journeymanapp-17b05.appspot.com/o/imagesNatalia%2F1641946321560.jpg?alt=media&token=17c803ac-7a22-44a6-9485-bec4f2f34c9a
-console.log({photos}) 
-  
-return (
-    <>
-        <h1>I am PhotoGallery componet</h1>
-        {/* <img 
-      src="https://firebasestorage.googleapis.com/v0/b/journeymanapp-17b05.appspot.com/o/imagesNatalia%2F1641946321560.jpg?alt=media&token=17c803ac-7a22-44a6-9485-bec4f2f34c9a"
-      alt="new"
-        /> */}
-        <div> 
-            {photos.map((photo,i) =>
-            <img className={styles.photo} key={i} src={photo} />)}
-        </div>
-    </>    
-    )
+export default function PhotoGallery({ photos }) {
+    // for testing doll's url in FB https://firebasestorage.googleapis.com/v0/b/journeymanapp-17b05.appspot.com/o/imagesNatalia%2F1641946321560.jpg?alt=media&token=17c803ac-7a22-44a6-9485-bec4f2f34c9a
+    console.log({ photos });
+    return (
+        <Carousel>
+            {photos.map ( (photo, i) => (
+                        <Carousel.Item key={i}>
+                        <img
+                            className="d-block w-100"
+                            src={photos[i]}
+                            alt="Slide"
+                        />
+                        </Carousel.Item>
+            ))}
+        </Carousel>
+    );
 }
