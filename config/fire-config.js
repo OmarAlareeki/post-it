ad4827377c0dac15d06b14942badc68767e70740
NGFuture <78330887+NGFuture@users.noreply.github.com>
2022-01-16 22:02:54 -0800

    fire-config fixed

--- config/fire-config.js
+++ config/fire-config.js
@@ -4,26 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messgingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: "${config.measurementId}",
-// };
-
-//to upgrade to version 9
-const firebaseApp = initializeApp({ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messgingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: "${config.measurementId}", });
-
-const db = getFirestore(firebaseApp);
+  measurementId: "${config.measurementId}",
+};
 
 const app = initializeApp(firebaseConfig, )
 const db = getFirestore(app);
