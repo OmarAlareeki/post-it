f481ff925f3546944b457a754cefeafa9fe811f9
NGFuture <78330887+NGFuture@users.noreply.github.com>
2022-02-01 21:34:42 -0800

    updated savedposts

--- components/displaypage/Content.js
+++ components/displaypage/Content.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Content.module.css";
 import Popup from "./Popup";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../config/fire-config";
+import SignInPage from "../../pages/signIn/SignIn";
+import { doc, onSnapshot, collection, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import { getDatabase, ref, set } from "firebase/database";
+import { db, storage } from "../../config/fire-config";
 
 export default function Content({ post }) {
     // timestamp= {nanoseconds: 0,
@@ -8,30 +14,56 @@ export default function Content({ post }) {
     // console.log(new Date(timestamp.seconds*1000))
     console.log(new Date(post.postDate.seconds * 1000))
 
-    // todo take authentification 
-    const user = null
-    // {
-    //     userID: 1,
-    //     // savedPost: [{
-    //     //     postId: post.id
-    //     // }]
-    // }
 
+
+    const [currentUser, setCurrentUser] = useState("");
+    const [user, setUser] = useState(null);
     const [showContacts, setShowContacts] = useState(false);
+    const [loginAlert, setLoginAlert] = useState(false);
+    const [liked, setLiked] = useState(false);
+
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setCurrentUser(user);
+                // setLoggedIn(true);
+            } else {
+                setCurrentUser("");
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!currentUser) return
+        const docRef = doc(db, "users", currentUser.uid);
+        return onSnapshot(docRef, (doc) => {
+            const userDataBase = doc.data();
+            setUser(userDataBase);
+            setLiked(userDataBase && userDataBase.savedPost && !!userDataBase.savedPost.find((nextPost) => {
+                return (nextPost.postId === post.id)
+            }))
+        })
+    }, [currentUser])
+    
+    console.log({user})
+
     const handleContactClick = () => {
-        if (!user) {
+        if (!currentUser) {
             setLoginAlert(true);
             return
         }
         setShowContacts(true);
     }
 
-    const [loginAlert, setLoginAlert] = useState(false);
-    const [liked, setLiked] = useState(user && user.savedPost && !!user.savedPost.find((nextPost) => {
-        return (nextPost.postId === post.id)
-    }));
+    const updateSavedPosts = (savedPost) => {
+        updateDoc(doc(db, "users", user.uid), {
+            savedPost
+          })
+    }
+
     const handleLikeClick = () => {
-        if (!user) {
+        if (!currentUser) {
             setLoginAlert(true);
             return
         }
@@ -42,12 +74,21 @@ export default function Content({ post }) {
                 return (nextPost.postId === post.id);
             })
             newSavedPost.splice(postIndex, 1);
-            //todo send to FB
+            console.log({newSavedPost})
+            updateSavedPosts(newSavedPost);
             setLiked(false)
         } else {
             const newSavedPost = [...(user.savedPost || [])];
-            newSavedPost.push({ postId: post.id })
-            //todo send to FB
+            newSavedPost.push({ 
+                postId: post.id,
+                postDate: post.postDate,
+                imageUrls: post.imageUrls[0],
+                price: post.price,
+                title: post.title
+
+             })
+            console.log({newSavedPost});
+            updateSavedPosts(newSavedPost);
             setLiked(true)
         }
 
@@ -55,7 +96,7 @@ export default function Content({ post }) {
 
     return (
         <>
-            {loginAlert && <Popup close={()=>{setLoginAlert(false)}}>Please, login form</Popup>}
+            {loginAlert && <Popup close={() => { setLoginAlert(false) }}><SignInPage /></Popup>}
             <button onClick={handleLikeClick}>
                 {liked ? (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className="bi bi-heart" viewBox="0 0 16 16">
                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
@@ -69,7 +110,7 @@ export default function Content({ post }) {
             <p> Posted 3 days ago </p>
             <p>{post.description} </p>
             <button onClick={handleContactClick} className={styles.contactBtn}>Contact seller</button>
-            {showContacts && <p> {post.phone} <br/> {post.email} </p>}
+            {showContacts && <p> {post.phone} <br /> {post.email} </p>}
 
         </>
 
