d21a77be76592b9329c973b914a52a2c50ec8565
NGFuture <78330887+NGFuture@users.noreply.github.com>
2022-02-06 21:44:30 -0800

    some changes before merge

--- components/displaypage/DisplayPost.js
+++ components/displaypage/DisplayPost.js
@@ -2,30 +2,30 @@ import React from "react";
 import Content from "./Content";
 import PhotoGallery from "./PhotoGallery";
 import styles from "./DisplayPost.module.css";
-import dynamic from 'next/dynamic';
-import Router from "next/router"
+import dynamic from 'next/dynamic'
 const Map = dynamic(
-    () => import('./Map'),
-    { ssr: false }
+  () => import('./Map'),
+  { ssr: false }
 )
 
 export default function DisplayPost({ post }) {
     return (
-        <>
-            <div className={styles.mainField}>
-                <div className={styles.content}>
-                    <Content post={post} />
-                </div>
-                <div className={styles.map}>
-                    <Map zip={post.zip} title={post.title} price={post.price} />
-                </div>
-                <div className={styles.photoGallery}>
-                    <PhotoGallery photos={post.imageUrls} />
-                </div>
+        <div className={styles.mainField}>
+            <div className={styles.content}>
+                <Content post={post} />
             </div>
-        </>
+            <div className={styles.map}>
+                <Map zip={post.zip} title={post.title} price={post.price}/>
+            </div>
+            <div className={styles.photoGallery}>
+                <PhotoGallery photos={post.imageUrls} />
+            </div>
+        </div>
     )
 }
+
+
+
 
 
 
