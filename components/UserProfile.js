ac6db2086b72ba7b2fb2ed9f80ad1ac3f0d4ebe1
CodergirlVS <81403558+CodergirlVS@users.noreply.github.com>
2022-02-11 13:31:17 -0800

    Update UserProfile.js
    Updated the page with new CSS and code for Change Image. Vaidehi 2/11/22.

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -2,11 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { db, storage } from "../config/fire-config";
 import { RiCloseCircleFill } from "react-icons/ri";
-import { IoMdAddCircle } from "react-icons/io";
+import { IoMdCheckmarkCircle } from "react-icons/io";
 import { TailSpin } from "react-loader-spinner";
 import { Image } from "react-bootstrap";
-import PostsListContainer from "./PostsListContainer";
-//import DeleteConfirmation from "./DeleteConfirmation";
 import {
   ref,
   uploadBytesResumable,
@@ -20,7 +18,6 @@ import {
   collection,
   query,
   where,
-  deleteDoc,
   onSnapshot,
 } from "firebase/firestore";
 import style from "../styles/UserProfile.module.css";
@@ -38,19 +35,13 @@ import {
   Typography,
 } from "@material-ui/core";
 import PasswordIcon from "@mui/icons-material/Password";
-import DeleteIcon from "@mui/icons-material/Delete";
 
-function UserProfile({ id, handleClick, setConfirmationMessage }) {
+function UserProfile({ id }) {
   const [user, setUser] = useState([]);
   const [postCount, setPostCount] = useState([]);
-  const [displayUrl, setDisplayUrl] = useState([]);
+  const [displayUrl, setDisplayUrl] = useState("");
   const [progress, setProgress] = useState("getUpload");
   const [showIcons, setShowIcons] = useState(false);
-  // const [dTitle, setDTitle] = useState("");
-  // const [uid, setUid] = useState(null);
-  // const [displayConfirmationModal, setDisplayConfirmationModal] =
-  //   useState(false);
-  // const [deleteMessage, setDeleteMessage] = useState(null);
 
   const postsRef = collection(db, "posts");
   const docRef = doc(db, "users", id);
@@ -117,45 +108,51 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
   const displayImage = (dUrl) => {
     return (
       <>
-        <div>
-          <Image
-            src={dUrl}
-            alt={dUrl}
-            height={200}
-            width={200}
-            className={style.userImage}
-            onClick={() => {
-              console.log("image clicked");
-            }}
-          />
-
-          <RiCloseCircleFill
-            style={{
-              fill: "orangered",
-              fontSize: "30px",
-              cursor: "pointer",
-            }}
-            onClick={(e) => deleteImage(dUrl)}
-          />
-        </div>
         {progress === "uploading" ? (
           <div className={style.loader}>
             <TailSpin color="#ef9d06" height={40} width={40} />
           </div>
         ) : (
-          <></>
+          <div>
+            <Image
+              src={dUrl}
+              alt={dUrl}
+              height={200}
+              width={200}
+              className={style.userImage}
+            />
+
+            <RiCloseCircleFill
+              style={{
+                fill: "orangered",
+                fontSize: "30px",
+                cursor: "pointer",
+              }}
+              onClick={() => deleteImage(dUrl)}
+            />
+
+            <IoMdCheckmarkCircle
+              style={{
+                fill: "green",
+                fontSize: "30px",
+                cursor: "pointer",
+              }}
+              onClick={(e) => handleSubmit()}
+            />
+          </div>
         )}
       </>
     );
   };
 
   const deleteImage = (dUrl) => {
-    setDisplayUrl(displayUrl.filter((imageurl) => imageurl !== dUrl));
+    setDisplayUrl("");
     const deleteRef = ref(storage, dUrl);
     deleteObject(deleteRef)
       .then(() => {
         console.log("picture deleted");
         console.log(displayUrl);
+        setProgress("getUpload");
         setShowIcons(false);
       })
       .catch((error) => {
@@ -163,19 +160,17 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
       });
   };
 
-  const handleSubmit = (event) => {
-    console.log(displayUrl);
-    event.preventDefault();
-    try {
-      docRef.updateDoc("photo", displayUrl);
-      setDisplayUrl([]);
-      setProgress("getUpload");
-      setShowIcons(false);
-    } catch {
-      (error) => {
+  const handleSubmit = () => {
+    updateDoc(docRef, { photo: displayUrl })
+      .then(() => {
+        setDisplayUrl("");
+        setProgress("getUpload");
+        setShowIcons(false);
+        window.location.reload();
+      })
+      .catch((error) => {
         console.error("Error adding Document: ", error);
-      };
-    }
+      });
   };
 
   return (
@@ -184,14 +179,15 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
         <Grid
           container
           spacing={2}
+          xs={12}
           direction="row"
-          justify="space-evenly"
+          justify="center"
           key={data.id}
         >
-          <Grid item xs={12} direction="row">
+          <Grid item xs={12}>
             <img src={data.photo} className={style.DisplayImagediv} />
             <div>
-              <FormControl onSubmit={(e) => handleSubmit(e)}>
+              <FormControl onSubmit={() => handleSubmit()}>
                 <label htmlFor="contained-button-file">
                   <Input
                     id="contained-button-file"
@@ -206,45 +202,26 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
                     component="span"
                     color="success"
                     justify="center"
+                    disabled={showIcons}
                     size="small"
                     sx={{ margin: 2, backgroundColor: "#ef9d06" }}
                   >
                     Change Photo
                   </Button>
-
-                  <div style={{ display: "flex" }}>
-                    {imageContent()}
-                    <div style={{ display: "flex" }}>
-                      <Button
-                        style={{
-                          cursor: "pointer",
-                          display: showIcons ? "block" : "none",
-                        }}
-                        type="submit"
-                      >
-                        <IoMdAddCircle
-                          style={{
-                            fill: "green",
-                            fontSize: "30px",
-                          }}
-                        />
-                      </Button>
-                    </div>
-                  </div>
                 </label>
               </FormControl>
+              <div style={{ display: "flex" }}>{imageContent()}</div>
             </div>
           </Grid>
 
-          <Grid item xs={12} sm container>
-            <TableContainer>
-              <Paper elevation={12} variant="outlined" maxwidth={600}>
+          <Grid item xs={12} className={style.DisplayCard}>
+            <TableContainer style={{ overflow: "hidden" }}>
+              <Paper variant="outlined">
                 <Typography
                   gutterBottom
                   variant="subtitle1"
                   fontWeight="bold"
                   fontSize="25px"
-                  p="10px"
                 >
                   {data.name}
                 </Typography>
@@ -264,25 +241,11 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
                     </TableRow>
 
                     <TableRow>
-                      <TableCell component="th" scope="row">
-                        <Typography variant="body1" gutterBottom fontSize={20}>
-                          Signup Method :
-                        </Typography>
-                      </TableCell>
-                      <TableCell align="center">
-                        <Typography variant="body2" gutterBottom fontSize={15}>
-                          {data.provider.split(".")[0].toUpperCase()}
-                        </Typography>
-                      </TableCell>
-                    </TableRow>
-
-                    <TableRow>
                       <TableCell colSpan={2}>
                         <Button
                           variant="outlined"
                           startIcon={<PasswordIcon />}
                           size="small"
-                          onClick
                         >
                           <Typography
                             sx={{ cursor: "pointer" }}
@@ -307,8 +270,6 @@ function UserProfile({ id, handleClick, setConfirmationMessage }) {
                             .toLocaleDateString()}
                         </Typography>
                       </TableCell>
-                      <TableCell />
-                      <TableCell />
                     </TableRow>
 
                     <TableRow>
@@ -373,7 +334,17 @@ export default UserProfile;
 }
 
 {
-  /* <Grid item xs={12}>
+  /* import PostsListContainer from "./PostsListContainer";
+//import DeleteConfirmation from "./DeleteConfirmation";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+  // const [dTitle, setDTitle] = useState("");
+  // const [uid, setUid] = useState(null);
+  // const [displayConfirmationModal, setDisplayConfirmationModal] =
+  //   useState(false);
+  // const [deleteMessage, setDeleteMessage] = useState(null);
+  
+  <Grid item xs={12}>
             <Button
               variant="outlined"
               startIcon={<DeleteIcon />}
