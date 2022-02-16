41afd1d39b9b267407baf2138cab8e129c56dbd8
Munir Nuristani <83889870+MunirNuristani@users.noreply.github.com>
2022-02-15 16:10:46 -0800

    Revert "Some work done"

--- components/displaypage/Content.js
+++ components/displaypage/Content.js
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Content.module.css";
-import {formatDay} from "../DaysAgo";
+import {
+  doc,
+  onSnapshot,
+  collection,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { getDatabase, ref, set } from "firebase/database";
+import { db, storage } from "../../config/fire-config";
+import DaysAgo, {formatDay} from "../DaysAgo";
 import ShareBtn from "./ShareBtn"
 import Button from '@mui/material/Button';
-import { useRouter } from "next/router";
 
 
 export default function Content({ post, setLoginAlert, currentUser }) {
+  
   const [showContacts, setShowContacts] = useState(false);
-  const router = useRouter();
+
   const handleContactClick = () => {
     if (!currentUser) {
-      router.push({
-          pathname: "/signIn/SignIn",
-          query: {
-            routeTo: `displaypage/${post.id}`}
-        })
-    } else{
-      setShowContacts(true)
+      setLoginAlert(true);
+      return;
     }
+    setShowContacts(true);
   };
 
  
