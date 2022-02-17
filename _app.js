510526ac91f60cc2424c8572be99b7b2bf883480
NGFuture <78330887+NGFuture@users.noreply.github.com>
2022-02-16 22:27:16 -0800

    merging master 2

---
+++ _app.js
@@ -0,0 +1,46 @@
+import "../styles/globals.css";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../components/mui/theme";
+import createEmotionCache from "../components/mui/createEmotionCache";
+import MainLayout from "../components/MainLayout";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Post It </title>
+        <meta
+          name="description"
+          content="This app helps to keep track of orders"
+        />
+        <link rel="icon" href="/post_it.png" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
