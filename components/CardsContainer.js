790d68c66727e1f3370b1a2fd95419b0d3bb6d5b
CodergirlVS <vaidehis1214@gmail.com>
2022-02-21 12:35:07 -0800

    Cahnges made to cards, search and navbar

--- components/CardsContainer.js
+++ components/CardsContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import style from "../styles/Home.module.css";
@@ -12,57 +12,63 @@ const CardsContainer = ({
   handleClick,
 }) => {
   return (
-      <div className={style.PostsDisplay}>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <Card className={style.Cards}>
-              <Card.Link
-                href={`/displaypage/${post.id}`}
-                style={{
-                  color: "black",
-                  textDecoration: "none",
-                }}
-              >
-                <Card.Img
-                  variant="top"
-                  src={post.imageUrls}
-                  alt={post.title}
-                  className={style.CardImage}
-                />
-                <Card.Body>
-                  <Card.Title
-                    className="h6"
-                    style={{
-                      overflow: "hidden",
-                      whiteSpace: "nowrap",
-                      textOverflow: "ellipsis",
-                      textDecoration: "none",
-                    }}
-                  >
-                    {post.title}
-                  </Card.Title>
-                  <Card.Text>$ {post.price}</Card.Text>
-                  <Card.Text>
-                    <ZipToCity zip={post.zip} />
-                  </Card.Text>
-                  <Card.Text className={style.timeAgo}>
-                    {formatDay(post.postDate.seconds)}
-                  </Card.Text>
-                </Card.Body>
-              </Card.Link>
-            </Card>
-            <>
-              <DeletePost
-                handleClick={handleClick}
-                setConfirmationMessage={setConfirmationMessage}
-                pid={post.id}
-                title={post.title}
-                deleteBtnStatus={deleteBtnStatus}
+    <div className={style.PostsDisplay}>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Card className={style.Cards}>
+            <Card.Link
+              href={`/displaypage/${post.id}`}
+              style={{
+                color: "black",
+                textDecoration: "none",
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={post.imageUrls}
+                alt={post.title}
+                className={style.CardImage}
               />
-            </>
-          </div>
-        ))}
-      </div>
+              <Card.Body>
+                <Card.Title
+                  className="h6"
+                  style={{
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    textOverflow: "ellipsis",
+                    textDecoration: "none",
+                  }}
+                >
+                  {post.title}
+                </Card.Title>
+                {post.price === 0 ? (
+                  <Card.Text>Free</Card.Text>
+                ) : (
+                  <Card.Text style={{ marginBottom: "10px" }}>
+                    $ {post.price}
+                  </Card.Text>
+                )}
+                <Card.Text style={{ marginBottom: "0" }}>
+                  <ZipToCity zip={post.zip} />
+                </Card.Text>
+                <Card.Text className={style.timeAgo}>
+                  {formatDay(post.postDate.seconds)}
+                </Card.Text>
+              </Card.Body>
+            </Card.Link>
+          </Card>
+          <>
+            <DeletePost
+              handleClick={handleClick}
+              setConfirmationMessage={setConfirmationMessage}
+              pid={post.id}
+              title={post.title}
+              deleteBtnStatus={deleteBtnStatus}
+            />
+          </>
+        </div>
+      ))}
+    </div>
   );
 };
 export default CardsContainer;
