e881617bdefe49db9b2d64511d9164af30c80cc6
OmarAlareeki <omarproductions2018@gmail.com>
2022-02-22 17:59:10 -0800

    ReadMe file adjustment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
 
 ## Getting Started
 
@@ -8,7 +8,7 @@ First, run the development server:
 npm run dev
 # or
 yarn dev
-```
+
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
@@ -18,17 +18,59 @@ You can start editing the page by modifying `pages/index.js`. The page auto-upda
 
 The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
-## Learn More
+```
 
-To learn more about Next.js, take a look at the following resources:
+-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+General Information
+Team Name: Post-It
+Team Members: OmarAlareeki, CodergirlVS, MunirNuristani and NGFuture
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+- Project Overview and Concept
+Drawing inspiration from the UX of apps like OfferUp and LetGo, Post-It aims to give the user a way to find and sell goods.
 
-## Deploy on Vercel
-Check out live site
+
+- The Post-It Project’s mission is to enable a hyper-local gift economy, in order to curb needless consumption, engender human connection, and promote sustainability.
+
+- Our app contributes to this mission by making the experience of offering and accepting goods as safe and friction-free as possible.
+Minimum Viable Product
+
+- User stories:
+Since this is essentially a two-sided marketplace, we have two User personas, Listers (the “sellers”) and Searchers (“buyers”). (An interesting wrinkle here is that in some sense the Listers are themselves receiving a service—probing that may bear fruit.)
+
+    As a Lister, In order to keep my life clutter-free as easily as possible, I want to snap a photo of things I want to be rid of and have someone come pick it up to take it away.
+
+    As a Lister, I want to provides my services to the community as a way to hone my skills, connect with people as well as giving back to the community.
+
+    As a Searcher, In order to fill a specific need, I want to easily see listings for free items and details relevant to making a decision to invest the time into picking it up.
+
+
+- Stretch Goals (rough priority ordering)
+
+    Views count for a post
+    Category drop down menu
+    Chat between users
+    Video upload for a post
+    Scheduling / calendar integration
+    Adding expiration period for a post
+
+- Frameworks and Libraries
+
+    NextJs
+    Facebook Login
+    Google Login
+    Bootstrap
+    React Bootstrap
+    material-ui
+    react-lodash
+    react-leaflet
+
+  Testing:
+
+    Jest
+
+- ## Deploy on Vercel
+     Check out live site
 https://post-it-tau.vercel.app/
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
--- pages/userProfilePage/[uid].js
+++ pages/userProfilePage/[uid].js
@@ -1,49 +1,65 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { db, auth } from "../../config/fire-config";
+import { db, storage, auth } from "../../config/fire-config";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { IoMdCheckmarkCircle } from "react-icons/io";
+import { TailSpin } from "react-loader-spinner";
+import { Image } from "react-bootstrap";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import {
   doc,
   getDoc,
+  updateDoc,
   collection,
   query,
   where,
   onSnapshot,
+  docRef,
 } from "firebase/firestore";
 import style from "../../styles/UserProfile.module.css";
 import {
   Grid,
   Button,
+  Input,
+  FormControl,
   TableContainer,
   Table,
   TableBody,
   TableCell,
   TableRow,
+  Paper,
   Typography,
 } from "@material-ui/core";
 import PasswordIcon from "@mui/icons-material/Password";
 import { useRouter } from "next/router";
 import { onAuthStateChanged } from "firebase/auth";
-import ProfileImage from "../../components/UserProfilePage/ProfileImage";
 
 function UserProfile() {
   const router = useRouter();
-  const uid = router && router.query.uid;
-
-  const [user, setUser] = useState("");
+  const id = router && router.query.uid;
+  const [user, setUser] = useState([]);
   const [postCount, setPostCount] = useState([]);
+  const [displayUrl, setDisplayUrl] = useState("");
+  const [progress, setProgress] = useState("getUpload");
+  const [showIcons, setShowIcons] = useState(false);
 
   useEffect(async () => {
-    if (!uid) return false;
+    if (!id) return false;
     const postsRef = collection(db, "posts");
-    const docRef = doc(db, "users", uid);
+    const docRef = doc(db, "users", id);
     let q;
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       const userData = { ...docSnap.data(), id: docSnap.id };
-      setUser(userData);
+      setUser([userData]);
     }
 
-    q = query(postsRef, where("userId", "==", uid));
+    q = query(postsRef, where("userId", "==", id));
     onSnapshot(q, (snap) => {
       const queryList = snap.docs.map((doc) => ({
         id: doc.id,
@@ -51,69 +67,193 @@ function UserProfile() {
       }));
       setPostCount(queryList);
     });
-  }, [uid]);
-  console.log(user);
-
+  }, [id]);
   useEffect(()=>{
-    onAuthStateChanged(auth, (user) => (user ? "" : router.push("/")));
-  })
+    onAuthStateChanged(auth, (user) => (user ? "" : router.push("/")))
+  },[])
+  ;
+  const handleImageUpload = (e) => {
+    const userImage = e.target.files[0];
+    const imageRef = ref(storage, `userProfileImages/${userImage.name}`);
+    const uploadImages = uploadBytesResumable(imageRef, userImage);
+    uploadImages.on(
+      "state_changed",
+      (snapshot) => {
+        const process = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("uploading", process);
+        setProgress("uploading");
+      },
+      (error) => {
+        console.log("Encounter ", error);
+      },
+      () => {
+        getDownloadURL(
+          ref(storage, `userProfileImages/${userImage.name}`)
+        ).then((url) => {
+          setDisplayUrl(url);
+          console.log(url);
+          setProgress("uploaded");
+          setShowIcons(true);
+        });
+      }
+    );
+  };
+  const imageContent = () => {
+    switch (progress) {
+      case "getUpload":
+        return <div>Preview new photo here!</div>;
+      case "uploading":
+        return <div>{displayUrl ? displayImage(displayUrl) : ""}</div>;
+      case "uploaded":
+        return <div>{displayUrl ? displayImage(displayUrl) : ""}</div>;
+      case "failedUpload":
+        return <div> Upload failed </div>;
+    }
+  };
+
+  const displayImage = (dUrl) => {
+    return (
+      <>
+        {progress === "uploading" ? (
+          <div className={style.loader}>
+            <TailSpin color="#ef9d06" height={40} width={40} />
+          </div>
+        ) : (
+          <div>
+            <Image
+              src={dUrl}
+              alt={dUrl}
+              height={200}
+              width={200}
+              className={style.userImage}
+            />
+
+            <RiCloseCircleFill
+              style={{
+                fill: "#D50005",
+                fontSize: "30px",
+                cursor: "pointer",
+              }}
+              onClick={() => deleteImage(dUrl)}
+            />
+
+            <IoMdCheckmarkCircle
+              style={{
+                fill: "#008000",
+                fontSize: "30px",
+                cursor: "pointer",
+              }}
+              onClick={(e) => handleSubmit()}
+            />
+          </div>
+        )}
+      </>
+    );
+  };
+
+  const deleteImage = (dUrl) => {
+    setDisplayUrl("");
+    const deleteRef = ref(storage, dUrl);
+    deleteObject(deleteRef)
+      .then(() => {
+        setProgress("getUpload");
+        setShowIcons(false);
+      })
+      .catch((error) => {
+        console.error("error occurd: ", error);
+      });
+  };
+
+  const handleSubmit = () => {
+    const docRef = doc(db, "users", id);
+    updateDoc(docRef, { photo: displayUrl })
+      .then(() => {
+        setDisplayUrl("");
+        setShowIcons(false);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error adding Document: ", error);
+      });
+  };
+
   return (
-    <div>
-      <main className={style.UserProfileContainer}>
-        <div className={style.ImageBackgroundOvly}></div>
-        <div className={style.DataBox}>
-          <>
-            <ProfileImage photo={user.photo} />
-          </>
-          <>
-            <div className={style.DisplayCard}>
-              <TableContainer>
+    <main className={style.UserProfileContainer}>
+      {user?.map((data) => (
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="center"
+          key={data.id}
+        >
+          <Grid item xs={12} sm={6}>
+            <img src={data.photo} style={{ height: "175px", width: "175px" }} />
+            <div>
+              <FormControl onSubmit={() => handleSubmit()}>
+                <label htmlFor="contained-button-file">
+                  <Input
+                    id="contained-button-file"
+                    type="file"
+                    style={{ display: "none" }}
+                    onChange={(e) => {
+                      handleImageUpload(e);
+                    }}
+                  />
+                  <Button
+                    variant="contained"
+                    component="span"
+                    color="success"
+                    justify="center"
+                    disabled={showIcons}
+                    size="small"
+                    sx={{ margin: 2, backgroundColor: "#ef9d06" }}
+                  >
+                    Change Photo
+                  </Button>
+                </label>
+              </FormControl>
+              <div style={{ display: "flex" }}>{imageContent()}</div>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6} className={style.DisplayCard}>
+            <TableContainer style={{ overflow: "hidden" }}>
+              <Paper variant="outlined">
+                <Typography
+                  gutterBottom
+                  variant="subtitle1"
+                  fontWeight="bold"
+                  fontSize="25px"
+                >
+                  {data.name}
+                </Typography>
                 <Table aria-label="simple table">
                   <TableBody>
                     <TableRow>
-                      <TableCell
-                        component="th"
-                        colSpan={2}
-                        align="center"
-                        sx={{ borderBottom: "none" }}
-                      >
+                      <TableCell component="th" scope="row">
                         <Typography
+                          variant="body1"
                           gutterBottom
-                          variant="subtitle1"
-                          fontWeight="bold"
-                          fontSize="25px"
-                          align="center"
+                          // fontSize={20}
                         >
-                          {user.name}
+                          Email :
+                        </Typography>
+                      </TableCell>
+
+                      <TableCell align="center">
+                        <Typography
+                          variant="body2"
+                          gutterBottom
+                          // fontSize={15}
+                        >
+                          {data.email}
                         </Typography>
                       </TableCell>
                     </TableRow>
-
-                    <TableRow>
-                      <TableCell
-                        colSpan={2}
-                        component="th"
-                        scope="row"
-                        align="center"
-                        sx={{
-                          borderBottom: "none",
-                          whiteSpace: "normal",
-                          wordWrap: "break-word",
-                        }}
-                      >
-                        <Typography variant="body1" gutterBottom>
-                          {user.email}
-                        </Typography>
-                      </TableCell>
-                    </TableRow>
-
-                    {user.provider === "Post-It Signup" ? (
+                    {data.provider === "Post-It Signup" ? (
                       <TableRow>
-                        <TableCell
-                          colSpan={2}
-                          align="center"
-                          sx={{ borderBottom: "none" }}
-                        >
+                        <TableCell colSpan={2}>
                           <Button
                             variant="outlined"
                             startIcon={<PasswordIcon />}
@@ -136,55 +276,80 @@ function UserProfile() {
                     )}
 
                     <TableRow>
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        align="center"
-                        sx={{ borderBottom: "none" }}
-                      >
-                        <Typography variant="body1" gutterBottom>
-                          Saved Post : {user?.savedPosts?.length}
+                      <TableCell component="th" scope="row">
+                        <Typography
+                          variant="body1"
+                          gutterBottom
+                          // fontSize={20}
+                        >
+                          Account Creation Date :
                         </Typography>
                       </TableCell>
 
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        align="center"
-                        sx={{ borderBottom: "none" }}
-                      >
-                        <Typography variant="body1" gutterBottom>
-                          My Posts : {postCount.length}
+                      <TableCell align="center">
+                        <Typography
+                          variant="body2"
+                          gutterBottom
+                          // fontSize={15}
+                        >
+                          {data.accountCreatedDate
+                            .toDate()
+                            .toLocaleDateString()}
                         </Typography>
                       </TableCell>
                     </TableRow>
 
                     <TableRow>
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        colSpan={2}
-                        align="center"
-                        sx={{ borderBottom: "none" }}
-                      >
-                        <Typography variant="body1" gutterBottom>
-                          Account since :{" "}
-                          {user.accountCreatedDate
-                            ?.toDate()
-                            .toLocaleDateString()}
+                      <TableCell component="th" scope="row">
+                        <Typography
+                          variant="body1"
+                          gutterBottom
+                          // fontSize={20}
+                        >
+                          Saved Post :
+                        </Typography>
+                      </TableCell>
+
+                      <TableCell align="center">
+                        <Typography
+                          variant="body2"
+                          gutterBottom
+                          // fontSize={15}
+                        >
+                          {data.savedPosts.length}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                      <TableCell component="th" scope="row">
+                        <Typography
+                          variant="body1"
+                          gutterBottom
+                          // fontSize={20}
+                        >
+                          My Posts :
+                        </Typography>
+                      </TableCell>
+
+                      <TableCell align="center">
+                        <Typography
+                          variant="body2"
+                          gutterBottom
+                          // fontSize={15}
+                        >
+                          {postCount.length}
                         </Typography>
                       </TableCell>
                     </TableRow>
                   </TableBody>
                 </Table>
-              </TableContainer>
-            </div>
-          </>
-        </div>
-        <div className={style.DivbottomMid}></div>
-      </main>
-      <div className={style.DivBottomTrapez}></div>
-    </div>
+              </Paper>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      ))}
+    </main>
   );
 }
 export default UserProfile;
