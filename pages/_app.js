0c3bd4d1e932350c1135fe8229182d9879544523
OmarAlareeki <omarproductions2018@gmail.com>
2022-02-24 17:41:27 -0800

    adding favicon

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ export default function MyApp(props) {
           content="This app helps to keep track of orders"
         />
         <link rel="icon" href="/new_logo.png" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
